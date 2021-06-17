edfa04718ed8f3d1311437d6a43cbf5c74bf9d7a
igor-ifrs <icc@pop-os.localdomain>
2021-06-17 13:22:47 -0300

    First commit :rocket::rocket::rocket:

---
+++ components/button/button.jsx
@@ -0,0 +1,6 @@
+import styles from './Button.module.css'
+export default function Btn() {
+    return(
+        <button className={styles.button} type="button">Botão</button>
+    )
+}
---
+++ pages/paraSuaEmpresa/index.jsx
@@ -0,0 +1,11 @@
+import Btn from "../../components/button/button"
+
+export default function paraSuaEmpresa(){
+    return(
+        <div>
+            <h1>teste</h1>
+            <Btn/>
+        
+        </div>
+    )
+}
